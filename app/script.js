d59223acc8b6efa4c31e4d4a3842f3466360eabc
yash <yashgulati.g1@gmail.com>
2016-12-14 11:53:57 +0530

     

--- app/script.js
+++ app/script.js
@@ -1,22 +1,34 @@
 $(document).ready(function(){
-  initiateSections();
+  // loadDocElements();
+  initiateVars();
   // eventHandlers();
 });
 
 var sec = [];
 var cross = [];
 
-function initiateSections(){
+function loadDocElements(){
+  var left = 33.3333 ;
+  for(var i=2;i<=9;i++){
+    $('.cross:nth-child('+i+')').css({'background-color': 'green','left': left+'%','top': '33.33%'});
+    left += left;
+    if(left>100) left = 33.3333;
+  }
+}
+
+function initiateVars(){
   for(var i=0;i<9;i++){
     sec.push($('.sec:nth-child('+(i+1)+')'));
     cross.push($('.cross:nth-child('+(i+1)+')'));
-    sec[i].click(function(){
-      cross[i].css('display','block');
-      this.css('background-color','#000000');
-    });
   }
-  cross[0].click(function(){
-    cross[0].css('display','none');
+  $('.cross').click(function(){
+    var index = $(this).index();
+    cross[index].css('display','none');
+  });
+  $('.sec').click(function(){
+    var index = $(this).index();
+    console.log(index);
+    cross[index].css('display','block');
   });
 
 // for(var i=0;i<9;i++){
