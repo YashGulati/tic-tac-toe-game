6136b405227fe0a0f12e9cd8956c9df9c107da3a
yash <yashgulati.g1@gmail.com>
2016-12-14 10:59:29 +0530

     

--- app/script.js
+++ app/script.js
@@ -12,12 +12,18 @@ function initiateSections(){
     sec.push($('.sec:nth-child('+(i+1)+')'));
     cross.push($('.cross:nth-child('+(i+1)+')'));
   }
-
-for(var i=0;i<9;i++){
-  sec[i].click(function(){
-    alert(i+1);
+  sec[0].click(function(){
+    alert('sec');
   });
-}
+  cross[0].click(function(){
+    alert('cross');
+  });
+
+// for(var i=0;i<9;i++){
+//   sec[i].click(function(){
+//     cross[i].toggle();
+//   });
+// }
 
   // sec[1].click(function(){
   //   alert('2');
