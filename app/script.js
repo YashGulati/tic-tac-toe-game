9efd89c27b6f7024089e3dec2f30122e6bbfefab
yash <yashgulati.g1@gmail.com>
2016-12-14 10:53:38 +0530

     

--- app/script.js
+++ app/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-  eventHandlers();
+  initiateSections();
+  // eventHandlers();
 });
 
-function eventHandlers(){
-  $('.sec').click(function(){
-    
+var sec = [];
+var cross = [];
+var num = [];
+
+function initiateSections(){
+  for(var i=0;i<9;i++){
+    sec.push($('.sec:nth-child('+(i+1)+')'));
+    cross.push($('.cross:nth-child('+(i+1)+')'));
+  }
+
+for(var i=0;i<9;i++){
+  sec[i].click(function(){
+    alert(i+1);
   });
+}
+
+  // sec[1].click(function(){
+  //   alert('2');
+  // });
 }
