7651611e9e65c4dcb229d2676b5130f86f8eec36
yash <yashgulati.g1@gmail.com>
2016-12-14 11:32:46 +0530

     

--- app/script.js
+++ app/script.js
@@ -10,10 +10,11 @@ function initiateSections(){
   for(var i=0;i<9;i++){
     sec.push($('.sec:nth-child('+(i+1)+')'));
     cross.push($('.cross:nth-child('+(i+1)+')'));
+    sec[i].click(function(){
+      cross[i].css('display','block');
+      this.css('background-color','#000000');
+    });
   }
-  sec[0].click(function(){
-    cross[0].css('display','block');
-  });
   cross[0].click(function(){
     cross[0].css('display','none');
   });
